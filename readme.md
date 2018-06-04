f7e1a23bce9e81774cb17b5b5acff55e5ec3e0e7
Angush (PC) <angus.stienlehmann@gmail.com>
2018-06-04 23:30:53 +1000

    Added user matching between Discord & Trello for additional metadata, optional configurations support, appended strings, additional logging, the createCard event, and delayed Trello initialization until the Discord bot was ready.

--- readme.md
+++ readme.md
@@ -1,36 +1,40 @@
 # Trellobot
-### *A simple Discord bot to log and report events in your Trello boards.*
+A simple Discord bot to log and report events from your Trello boards in your Discord server.
 
 ## Setup
 1. Clone repository.
 2. Run `npm install`.
-3. Configure `conf.json` file as desired ([see below](#conf.json)).
-4. Generate tokens and set up `.auth` file ([see below](#.auth)).
+3. Configure `conf.json` file as desired ([see below](#confjson)).
+4. Generate tokens and set up `.auth` file ([see below](#auth)).
 5. All done. Run Trellobot with `node trellobot.js`.
 
 ## conf.json
-There are several important values in here which inform Trellobot's operation. Here's what they're all for, and how to set them:
+There are several important values in here which inform Trellobot's operation. Here's what they're all for, and how to set them.
 
-Key           | Explanation
-------------- | -----------
-boardIDs      | An array of board IDs (strings) determining on which boards Trellobot reports. IDs can be extracted from the URLs of your Trello boards. (eg. the board ID for [https://trello.com/b/**HF8XAoZd**/welcome-board](https://trello.com/b/HF8XAoZd/welcome-board) is `HF8XAoZd`).
-serverID      | An ID string determining which Discord server Trellobot uses. Enable developer mode in Discord and right click a server icon to copy its ID.
-channelID     | An ID string determining which channel on your Discord server Trellobot uses to post reports. Enable developer mode in Discord and right click a channel to copy its ID.
-pollInterval  | An integer determining how often (in milliseconds) Trellobot polls your boards for activity. 
-prefix        | A string determining the prefix for Trellobot commands in Discord. Currently unused.
-enabledEvents | An array of event names (strings) determining which events can be reported. If empty, all events are enabled. Event names can be found [in the Trello documenation](https://developers.trello.com/v1.0/reference#action-types).
+*(optional properties marked with * asterisks)*
+
+Property         | Explanation
+---------------- | -----------
+`boardIDs`       | An array of board IDs (strings) determining on which boards Trellobot reports. IDs can be extracted from the URLs of your Trello boards. (eg. the board ID for [https://trello.com/b/**HF8XAoZd**/welcome-board](https://trello.com/b/HF8XAoZd/welcome-board) is `HF8XAoZd`).
+`serverID`       | An ID string determining which Discord server Trellobot uses. Enable developer mode in Discord and right click a server icon to copy its ID.
+`channelID`      | An ID string determining which channel on your Discord server Trellobot uses to post reports. Enable developer mode in Discord and right click a channel to copy its ID.
+`pollInterval`   | An integer determining how often (in milliseconds) Trellobot polls your boards for activity. 
+`prefix`*        | A string determining the prefix for Trellobot commands in Discord. Currently unused. Defaults to `.` (period).
+`contentString`* | A string included posted alongside all embeds. If you'd like to ping a certain role every time the bot posts, for example, you would put that string here.
+`enabledEvents`* | An array of event names (strings) determining which events can be reported. If empty, all events are enabled. Event names can be found [in the Trello documenation](https://developers.trello.com/v1.0/reference#action-types).
+`userIDs`*       | An object mapping Discord IDs to Trello usernames, like so: `userIDs: {"TrelloUser": "1395184357104955", ...}`, so Trellobot can pull relevant user data from Discord.
 
 You can refer to the `conf.json` included in the repository for an example.
 
 ## .auth
 The `.auth-template` file is included to save you time, but you will need to create the keys and tokens yourself to run Trellobot. Here's how:
 
-Key          | Where to get it
------------- | ---------------
-discordToken | Create an app for Trellobot to work through on [Discord's developer site](https://discordapp.com/developers/applications/me/create), then create a bot user (below app description/icon) and copy the token.
-trelloKey    | Visit [this page](https://trello.com/1/appKey/generate) to generate your public Trello API key.
-trelloToken  | Visit `https://trello.com/1/connect?name=trello-events&response_type=token&expiration=never&key=YOURPUBLICKEY` (replacing `YOURPUBLICKEY` with the appropriate key) to generate a token that does not expire. Remove `&expiration=never` from the URL if you'd prefer a temporary token.
+Property       | How to get the value
+-------------- | ----------------------
+`discordToken` | Create an app for Trellobot to work through on [Discord's developer site](https://discordapp.com/developers/applications/me/create), then create a bot user (below app description/icon) and copy the token.
+`trelloKey`    | Visit [this page](https://trello.com/1/appKey/generate) to generate your public Trello API key.
+`trelloToken`  | Visit `https://trello.com/1/connect?name=Trellobot&response_type=token&expiration=never&key=YOURPUBLICKEY` (replacing `YOURPUBLICKEY` with the appropriate key) to generate a token that does not expire. Remove `&expiration=never` from the URL if you'd prefer a temporary token.
 
-That's all.
+That's all for now.
 
 *i know the name is lame*
--- trellobot.js
+++ trellobot.js
@@ -5,26 +5,11 @@ const auth = JSON.parse(fs.readFileSync('.auth'))
 const conf = JSON.parse(fs.readFileSync('conf.json'))
 let latestActivityID = fs.existsSync('.latestActivityID') ? fs.readFileSync('.latestActivityID') : 0
 
-bot.on('ready', () => {
-    let channel = bot.channels.get(conf.channelID)
-    if (!bot.guilds.get(conf.serverID)) {
-        console.log(`Server with ID "${conf.serverID}" not found! I can't function without a valid server and channel.\nPlease add the correct server ID to your conf file, or if the conf data is correct, ensure I have proper access.\nYou may need to add me to your server using this link:\n    https://discordapp.com/api/oauth2/authorize?client_id=453077635418030081&permissions=0&scope=bot`)
-        process.exit()
-    } else if (!channel) {
-        console.log(`Channel with ID "${conf.channelID}" not found! I can't function without a valid channel.\nPlease add the correct channel ID to your conf file, or if the conf data is correct, ensure I have proper access.`)
-        process.exit()
-    }
-    conf.channel = channel
-    console.log(`== Bot logged in as ${bot.user.tag}. Ready for action, commander! ==`)
-    console.log(`Latest Activity ID: ${latestActivityID} (you can ignore this)`)
-})
-bot.login(auth.discordToken)
-
 const Trello = require('trello-events')
 const events = new Trello({
     pollFrequency: conf.pollInterval, // milliseconds
     minId: latestActivityID, // auto-created and auto-updated
-    start: true,
+    start: false,
     trello: {
         boards: conf.boardIDs, // array of Trello board IDs 
         key: auth.trelloKey, // your public Trello API key
@@ -32,11 +17,104 @@ const events = new Trello({
     } 
 })
 
-const send = (content, options = null) => conf.channel.send(content, options).catch(err => console.error(err))
+/*
+** =====================================
+** Discord event handlers and functions.
+** =====================================
+*/
 
-const eventEnabled = (type) => conf.enabledEvents ? conf.enabledEvents.includes(type) : true
+bot.login(auth.discordToken)
+bot.on('ready', () => {
+    let guild = bot.guilds.get(conf.serverID)
+    let channel = bot.channels.get(conf.channelID)
+    if (!guild) {
+        console.log(`Server with ID "${conf.serverID}" not found! I can't function without a valid server and channel.\nPlease add the correct server ID to your conf file, or if the conf data is correct, ensure I have proper access.\nYou may need to add me to your server using this link:\n    https://discordapp.com/api/oauth2/authorize?client_id=453077635418030081&permissions=0&scope=bot`)
+        process.exit()
+    } else if (!channel) {
+        console.log(`Channel with ID "${conf.channelID}" not found! I can't function without a valid channel.\nPlease add the correct channel ID to your conf file, or if the conf data is correct, ensure I have proper access.`)
+        process.exit()
+    } else if (!conf.boardIDs || conf.boardIDs.length < 1) {
+        console.log(`No board IDs provided! Please add at least one to your conf file. Check the readme if you need help finding a board ID.`)
+    }
+    conf.guild = guild
+    conf.channel = channel
+    if (!conf.contentString) conf.contentString = ""
+    if (!conf.enabledEvents) conf.enabledEvents = []
+    if (!conf.userIDs) conf.userIDs = {}
+    // set default prefix is none provided in conf
+    if (!conf.prefix) {
+        conf.prefix = "."
+        fs.writeFileSync('conf.json', JSON.stringify(conf, null, 4), (err, data) => console.log(`Updated conf file with default prefix ('.')`))
+    }
+    logInitializationData()
+    console.log(`== Bot logged in as @${bot.user.tag}. Ready for action! ==`)
+    events.start()
+})
 
+bot.on('message', (msg) => {
+    if (msg.channel.type !== "text") return 
+    if (msg.content.startsWith('.ping')) send({embed:getEmbedBase()})
+})
+
+/*
+** ====================================
+** Trello event handlers and functions.
+** ====================================
+*/
+
+// Fired when a card is created
+events.on('createCard', (event, board) => {
+    if (!eventEnabled(event.type)) return
+    logEventFire(event)
+    let embed = getEmbedBase(event)
+    .setTitle(`New card created under __${event.data.list.name}__!`)
+    .setDescription(`**TITLE:** ${event.data.card.name} — **[CARD LINK](https://trello.com/c/${event.data.card.shortLink})**\n\n**CREATED BY:** ${event.memberCreator.username}`)
+    send(addDiscordUserData(embed, event))
+}) 
+
+
+
+
+/*
+** =======================
+** Miscellaneous functions
+** =======================
+*/
 events.on('maxId', (id) => {
+    if (latestActivityID == id) return
     latestActivityID = id
     fs.writeFileSync('.latestActivityID', id)
 })
+
+const send = (options) => conf.channel.send(conf.contentString, options).catch(err => console.error(err))
+
+const eventEnabled = (type) => conf.enabledEvents ? conf.enabledEvents.includes(type) : true
+
+const logEventFire = (event) => console.log(`${new Date(event.date).toUTCString()} - ${event.type} fired`)
+
+const getEmbedBase = (event) => new Discord.RichEmbed()
+        .setFooter(conf.guild.members.get(bot.user.id).displayName, bot.user.displayAvatarURL)
+        .setColor("#127ABD")
+        .setTimestamp(event.date ? event.date : Date.now())
+
+const addDiscordUserData = (embed, event) => {
+    if (conf.userIDs[event.memberCreator.username]) {
+        let discordUser = conf.guild.members.get(conf.userIDs[event.memberCreator.username])
+        if (discordUser) {
+            embed.setThumbnail(discordUser.user.displayAvatarURL)
+            embed.setDescription(embed.description.replace(event.memberCreator.username, `${event.memberCreator.username} / ${discordUser.toString()}`))
+        }
+    }
+    return {embed} 
+}
+
+const logInitializationData = () => console.log(`== INITIALIZING WITH:
+    latestActivityID - ${latestActivityID}
+    boardIDs --------- ${conf.boardIDs.length + " [" + conf.boardIDs.join(", ") + "]"}
+    serverID --------- ${conf.serverID} (${conf.guild.name})
+    channelID -------- ${conf.channelID} (#${conf.channel.name})
+    pollInterval ----- ${conf.pollInterval} ms (${conf.pollInterval / 1000} seconds)
+    prefix ----------- "${conf.prefix}"${conf.prefix === "." ? " (default)" : ""}
+    contentString ---- ${conf.contentString !== "" ? "\"" + conf.contentString + "\"" : "none"}
+    enabledEvents ---- ${conf.enabledEvents.length > 0 ? conf.enabledEvents.length + " [" + conf.enabledEvents.join(", ") + "]" : "all"}
+    userIDs ---------- ${Object.getOwnPropertyNames(conf.userIDs).length}`)
